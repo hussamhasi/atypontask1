01bab81e842b03629fb2eac2493b9db736b88537
Hussam Hasi <hussamhasi@gmail.com>
2020-04-23 20:48:12 +0300

    this is first commmit.

---
+++ src/universitrycourses.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class universitrycourses {
+    List<String> students=new ArrayList();
+     String instuctorname;
+     String coursename;
+
+     universitrycourses(String instructorname,String coursename){
+         this.instuctorname=instructorname;
+         this.coursename=coursename;
+     }
+     void registration(String studentname){
+         students.add(studentname);
+     }
+
+}
